327b7d8b0aff9cb40c086fd44788baf42a4fa34d
imr1012 <imr1012@alu.ubu.es>
2025-11-24 00:06:07 +0100

    Finalizada aplicación

--- app.R
+++ app.R
@@ -8,22 +8,23 @@ library(leaflet)
 library(dplyr)
 library(rjson)
 library(mapSpain)
+library(ggplot2)
 
 
-#gbif_anual <- load("gbif_datos.Rdata")
-#gbif_meds <- load("gbif_meds.Rdata")
-#gbif_casos <- load("")
+load("INPUT/DATA/gbif_tabla.Rdata")
+load("INPUT/DATA/gbif_meds.Rdata")
+load("INPUT/DATA/tabla_casos.Rdata")
 
 spain <- esp_get_prov()
 can <- esp_get_can_box()
   
-gbif_local <- merge(spain, gbif_df, by.x = "ine.prov.name", by.y = "provincia")
+gbif_local <- merge(spain, gbif_tabla, by.x = "ine.prov.name", by.y = "Provincia")
 
 ui <- page_sidebar(
   title = "Biodiversidad",
   sidebar = sidebar(
-    title = "Opciones",
-    helpText("Crea mapas de biodiversidad con distintas variables."),
+    title = "Dashboard",
+    helpText("¡Estudia los efectos de la biodiversidad en la salud humana!"),
     selectInput(
       "var",
       label = "Elige qué quiere visualizar:",
@@ -38,23 +39,28 @@ ui <- page_sidebar(
   ),
   page_fillable(
     layout_columns(
-      card("Total de Especies"),
-      card("Total de Hospitalizados por Infección"),
-      card("Consumo de Fármacos"),
-      col_widths = c(3, 3, 5.5),
-      row_heights = c(1, 1, 2)
-    ),
-    layout_columns(
+      value_box(
+        title = "TOTAL DE",
+        value = textOutput("n_riqueza"),
+        "ESPECIES REGISTRADAS EN EL AÑO SELECCIONADO",
+        showcase = icon("otter", class = "light")
+      ),
+      value_box(
+        title = "TOTAL DE",
+        value = textOutput("pacientes"),
+        "HOSPITALIZADOS POR INFECCIÓN O ENFERMEDAD RESPIRATORIA",
+        showcase = icon("hospital", class = "regular")
+      ),
       card(
         card_header("Visto en un Mapa..."),
         leafletOutput("map")
       ),
       card(
-        card_header("Índices de Diversidad"),
-        leafletOutput("gbif_tabla")
+        card_header("Variación en el Consumo de Fármacos"),
+        plotOutput("medicacion")
       ),
-      col_widths = c(7, 5),
-      row_heights = c(2, 1)
+      col_widths = c(6,6,7,5),
+      row_heights = c(1, 4)
     )
   )
 )
@@ -62,8 +68,22 @@ ui <- page_sidebar(
 server <- function(input, output){
   selectedYear <- reactive({
     gbif_local %>%
-      filter(year == input$year)
+      filter(Año == input$year)
   })
+  
+  selectedYearPac <- reactive({
+    df_casos %>%
+      filter(Año == input$year)
+  })
+  
+  output$n_riqueza = renderText({
+    sum(selectedYear()$n_especies, na.rm = TRUE)
+  })
+  
+  output$pacientes = renderText({
+    sum(selectedYearPac()$Altas, na.rm = TRUE)
+  })
+  
   output$map = renderLeaflet({
     datos <- selectedYear()
     variable <- switch(
@@ -93,6 +113,38 @@ server <- function(input, output){
         position = "bottomright",
         opacity = 1
       )
+  })
+  
+  output$medicacion = renderPlot({
+    validate(
+      need(input$year > 2005 & input$year <= 2018,
+           "No hay datos de consumo de fármacos para este año")
+    )
+    
+    datos <- gbif_meds %>%
+      filter(Año == input$year) %>%
+      mutate(
+        delta_num = gsub(" %", "", `X..Δ`),        # Quitar " %"
+        delta_num = gsub(",", ".", delta_num),     # Cambiar coma por punto
+        delta_num = as.numeric(delta_num)          # Convertir a numérico
+      ) %>%
+      group_by(CCAA) %>%
+      summarise(delta_promedio = mean(delta_num, na.rm = TRUE))
+    
+    ggplot(datos, aes(x = CCAA, y = delta_promedio)) +
+    geom_bar(stat = "identity", fill = "deeppink") +
+    scale_y_continuous(
+      limits = c(-10, 10),           # De -10% a 10%
+      breaks = seq(-10, 0, 2)      # Saltos cada 10%
+    ) +
+    labs(
+      x = "Comunidad Autónoma",
+      y = "X..Δ"
+    ) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1),
+          axis.text.y = element_text(size = 6),
+    )
   })
 }
 
